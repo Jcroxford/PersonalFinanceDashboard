bf73642b967d0604724b42a415b30c6350cd62a3
Jcroxford <takeflightsc@gmail.com>
2022-01-16 00:10:59 -0700

    add session middelware to express app. & refactor server to express

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,75 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
-import 'reflect-metadata'
+import { buildSchemaSync } from 'type-graphql'
 import { createConnection } from 'typeorm'
-import { User } from './entity/User'
+import express from 'express'
+import session from 'express-session'
+import 'reflect-metadata'
+
+import { AuthResolvers } from './resolvers/authResolvers'
+import { UserResolvers } from './resolvers/userResolvers'
+import { verifiyAuthorized } from './middleware/requireAuth'
+
 require('dotenv').config()
+
+const app = express()
+
+app.use(
+  session({
+    store: new (require('connect-pg-simple')(session))({
+      createTableIfMissing: true,
+      conObject: {
+        user: 'postgres',
+        password: '',
+        host: 'localhost',
+        port: 5432,
+        database: 'PersonalFinanceDashboard'
+      }
+    }),
+    secret: process.env.SESSION_SECRET as string,
+    name: 'qid',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 * 10 * 365 // default 10 years
+    }
+  })
+)
 
 createConnection()
   .then(async connection => {
-    console.log('Inserting a new user into the database...')
-    const user = new User()
-    user.firstName = 'Timber'
-    user.lastName = 'Saw'
-    user.age = 25
-    await connection.manager.save(user)
-    console.log('Saved a new user with id: ' + user.id)
+    console.log('\nserver connected to database successfully')
 
-    console.log('Loading users from the database...')
-    const users = await connection.manager.find(User)
-    console.log('Loaded users: ', users)
+    const apolloServer = new ApolloServer({
+      schema: buildSchemaSync({
+        resolvers: [
+          AuthResolvers,
+          UserResolvers
+        ],
+        authChecker: verifiyAuthorized
+      }),
+      plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground()
+      ],
+      context: ({ req, res }) => ({ req, res }),
+      debug: process.env.NODE_ENV === 'development'
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({ app })
 
-    console.log('Here you can setup and run express/koa/any other framework.')
-  }).catch(error => console.log(error))
+    app.listen({ port: process.env.PORT }, () => {
+      console.log(`🚀  Server ready at http://localhost:${process.env.PORT}`)
+    })
+  })
+  .catch(console.error)
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin'
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster'
-  }
-]
-
-const resolvers = {
-  Query: {
-    books: () => books
-  }
-}
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground()
-  ]
-})
-
-server.listen({ port: 4000 }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
+// todo
+/**
+ * setup auth
+ * reset password with current password mutation
+ * reset forgotten password
+ * change db config to url? for typeorm and pg simple session
+ * change validation error style to match unautheorized error style
+ */
